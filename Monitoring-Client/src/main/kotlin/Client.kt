cd69d2e8d3bb3eaa3966c1671f8198daea4408ed
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2021-12-08 02:21:09 +0100

    update client

--- Monitoring-Client/src/main/kotlin/Client.kt
+++ Monitoring-Client/src/main/kotlin/Client.kt
@@ -1,31 +1,52 @@
-import kotlinx.html.div
-import kotlinx.html.dom.append
-import org.w3c.dom.Node
 import kotlinx.browser.document
-import kotlinx.browser.window
+import kotlinx.html.*
+import kotlinx.html.dom.create
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import org.w3c.dom.WebSocket
+import org.w3c.dom.*
+import kotlin.math.roundToInt
 
 fun main() {
-    window.onload = { document.body?.sayHello() }
     var ws = WebSocket("ws://localhost:1234/api/ws/data")
     ws.onclose = {
         ws = WebSocket("ws://localhost:1234/api/ws/data")
         null
     }
+    document.body?.append(createCpuInfo(CpuData(
+        0,
+        .0,
+        213123132,
+        124124141,
+        .4,
+        1.5
+    )))
     ws.onmessage = {
-        Json.decodeFromString<Data>(it.data as String)
-    }
-}
-
-fun cpuInfo() {
-}
-
-fun Node.sayHello() {
-    append {
-        div {
-            +"Hello from JS"
+        val data = Json.decodeFromString<Data>(it.data as String)
+        data.cpu.forEach {
+            document.body?.append(createCpuInfo(it))
         }
     }
 }
+
+fun createCpuInfo(data: CpuData): HTMLElement =
+    document.create.div {
+        div {
+            p(classes = "mhz") {
+                + (data.clock / 1000000F).toString()
+            }
+            div(classes = "circle-wrap") {
+                style = "--rot: ${data.usage * 180}deg"
+                div(classes = "circle") {
+                    div(classes = "mask full") {
+                        div(classes = "fill")
+                    }
+                    div(classes = "mask half") {
+                        div(classes = "fill")
+                    }
+                    div(classes = "inside-circle") {
+                        + "${(data.usage * 1000).roundToInt() / 10F}%"
+                    }
+                }
+            }
+        }
+    }
